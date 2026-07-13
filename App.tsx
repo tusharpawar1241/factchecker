0c2a12732d41ff244ffb5a6463008a17e04b7e35
Tushar Pawar <tusharpawar1241@gmail.com>
2026-07-13 18:08:34 +0530

    feat: integrate Vercel Speed Insights to monitor app performance

--- App.tsx
+++ App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import Dashboard from './components/Dashboard';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
-const App: React.FC = () => <Dashboard />;
+const App: React.FC = () => (
+  <>
+    <Dashboard />
+    <SpeedInsights />
+  </>
+);
 
 export default App;
